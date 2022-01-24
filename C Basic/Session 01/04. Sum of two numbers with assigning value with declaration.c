edd87ff0c74efda3bf71b9ab79dff9365b56bd5d
Purnendu Talukder <ptsourav21@gmail.com>
2022-01-25 01:57:35 +0600

    Coding Practice

---
+++ C Basic/Session 01/04. Sum of two numbers with assigning value with declaration.c
@@ -0,0 +1,19 @@
+/*
+Write a program to sum of two numbers assigning value with declaration.
+
+Author: Purnendu Talukder
+Mail: ptsourav21@gmail.com
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int a=5, b=7, sum;
+    sum=a+b;
+
+    printf("Sum is: %d\n", sum);
+
+    return 0;
+}
---
+++ C Basic/Session 01/05.What is the output.c
@@ -0,0 +1,21 @@
+/*
+What is the output?
+
+Author: Purnendu Talukder
+Mail: ptsourav21@gmail.com
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int x, y;
+    x=2;
+    y=x;
+    x=3;
+
+    printf("Value of y is: %d\n", y);
+
+    return 0;
+}
