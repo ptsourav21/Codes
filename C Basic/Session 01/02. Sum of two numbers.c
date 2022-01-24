f1037b461792636b249b360b5eb2cb7cf1487597
Purnendu Talukder <ptsourav21@gmail.com>
2022-01-25 01:33:59 +0600

    Code Practice C Basic

---
+++ C Basic/Session 01/02. Sum of two numbers.c
@@ -0,0 +1,24 @@
+/*
+Write a program to sum of two numbers.
+
+Author: Purnendu Talukder
+Mail: ptsourav21@gmail.com
+
+*/
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    a=5;
+    b=7;
+    sum=a+b;
+
+    printf("Sum is: %d", sum);
+    printf("\n");
+
+    return 0;
+}
