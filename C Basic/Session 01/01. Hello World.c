d65b97b9f32e98382277bfb4381ecfb42510c3d9
Purnendu Talukder <ptsourav21@gmail.com>
2022-01-25 01:15:06 +0600

    Coding Practice

---
+++ C Basic/Session 01/01. Hello World.c
@@ -0,0 +1,14 @@
+/*
+Write a program to print "Hello World"
+
+Author: Purnendu Talukder
+Mail: ptsourav21@gmail.com
+
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
